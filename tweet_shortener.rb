9a3e022f403ec1bc40ab0a78dbbd751ad87581af
Albina Zakirova <zak1804@gmail.com>
2018-11-12 02:12:45 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def dictionary
+  hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+
+end
+
+def word_substituter(tweet)
+   tweetArray = tweet.split
+   tweetArray.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  tweetArray.join(" ")
+end
